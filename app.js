41ac860dcb9d776dc1d2fbed1d400e2035a8383c
Deep <sekharsmemories@gmail.com>
2022-06-22 01:22:47 +0530

    test

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV == "production")
   const path = require('path')
   const publicPath = path.join(__dirname, '/clientside/build');
   app.use(express.static(publicPath))
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
 }
