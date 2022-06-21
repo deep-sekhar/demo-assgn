304b917e74343a1455341b6e1346c3308e50b0e6
Deep <sekharsmemories@gmail.com>
2022-06-22 01:23:07 +0530

    hi

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV == "production")
   app.get('/', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
-}
+} 
 
 // server routes
 const AddData = require("./routes/AddData")
