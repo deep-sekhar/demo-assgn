f0b888a2647e83cc226dcaa33d794738b67ead0f
Deep <sekharsmemories@gmail.com>
2022-06-22 01:55:15 +0530

    rwa

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ if(process.env.NODE_ENV == "production")
   const path = require('path')
   const publicPath = path.join(__dirname, 'clientside/build');
   app.use(express.static(publicPath))
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(publicPath, 'index.html'));
-//  });
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 }
 
 // server routes
